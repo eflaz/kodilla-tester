56ca739c00782830413bb3025496402d8a85cf4a
Ewelina Flazinska <ewelinkus@gmail.com>
2023-04-26 13:12:30 +0200

    Main and MainExtended Classes added

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,7 @@
 
 public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+    public static void main(String[] args) {
+        String firstClassMessage = "Hello from FirstClass!";// [2]
+        System.out.println(firstClassMessage); // [3]
     }
 }
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main (String [] args) {
+        String myName = "Krysia";
+        String myJob = "Student";
+        int myAge = 18;
+        double height = 186;
+        char schoolClass = 'B';
+        boolean isGreatStudent = true;
+        System.out.println(myName);
+        System.out.println(myJob);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(schoolClass);
+        System.out.println(isGreatStudent);
+
+    }
+}
