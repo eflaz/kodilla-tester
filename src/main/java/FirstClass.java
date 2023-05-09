0ac636fe7168ff46f2dad292b1ff94ada5c68908
Ewelina Flazinska <ewelinkus@gmail.com>
2023-05-09 22:02:15 +0200

    Notebook class added

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,7 +1,17 @@
 
 public class FirstClass {                           // [1]
     public static void main(String[] args) {
-        String firstClassMessage = "Hello from FirstClass!";// [2]
-        System.out.println(firstClassMessage); // [3]
+        Notebook notebook = new Notebook(600, 150);
+        System.out.println("notebook.weight: " + notebook.weight);
+        System.out.println("notebook.price: " + notebook.price);
+
+        Notebook heavyNotebook = new Notebook(2000, 200);
+        System.out.println(heavyNotebook.weight + heavyNotebook.price);
+
+
+        Notebook oldNotebook = new Notebook(1200, 300);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+
+
     }
 }
---
+++ src/main/java/Notebook.java
@@ -0,0 +1,9 @@
+public class Notebook {
+   int weight;
+   int price;
+    public Notebook(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+
+    }
+}
