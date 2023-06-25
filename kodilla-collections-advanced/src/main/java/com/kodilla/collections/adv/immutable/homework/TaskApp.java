8bf3ad422ce456e4d524ed7e8420199acbacc767
Ewelina Flazinska <ewelinkus@gmail.com>
2023-06-25 15:27:09 +0200

    modul 5.3 homework added

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApp.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApp {
+    public static void main(String[] args) {
+        TaskRecord taskRecord = new TaskRecord("homework", 45);
+    }
+}
